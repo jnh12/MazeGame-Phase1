00d1d703047eaf4dc58f4568b716694662319d0f
schedid <chedidstephane@hotmail.com>
2024-03-19 23:29:33 +0200

    function compute frontier cell and added comments

--- MazeGenerator.hpp
+++ MazeGenerator.hpp
@@ -22,23 +22,54 @@ public:
         arr.assign(rows, vector<int>(cols, BLOCKED)); //fills array with blocked
     }
 
-    pair<int, int> pickRandomCell() { // locates radnom cell in array (if u want to use pair outpput use .first/.second)
+    pair<int, int> pickRandomCell() { // locates random cell in array (if u want to use pair output use .first/.second)
         srand(time(0));
         int randomRow = rand() % rows;
         int randomCol = rand() % cols;
         return {randomRow, randomCol};
     }
 
-    vector<pair<int, int>> computeFrontierCells(pair<int, int> cell); // implement finding of frontier cells. Cell inputted as pair and then return an array that contains frontier cells
+    vector<pair<int, int>> computeFrontierCells(pair<int, int> cell) {
+        pair<int, int> top = {arr[cell.first - 2], arr[cell.second]};
+        pair<int, int> right = {arr[cell.first], arr[cell.second + 2]};
+        pair<int, int> bottom = {arr[cell.first + 2], arr[cell.second]};
+        pair<int, int> left = {arr[cell.first], arr[cell.second - 2]};
+        vector<pair<int,int>> frontiers;
 
-    void generateMaze(){
+        if (top.first >= 0 && arr[top.first][top.second] == BLOCKED) {
+            frontiers.push_back(top);
+        }
+
+        if (right.second < cols && arr[right.first][right.second] == BLOCKED){
+            frontiers.push_back(right);
+        }
+
+        if (bottom.first < rows && arr[bottom.first][bottom.second] == BLOCKED){
+            frontiers.push_back(bottom);
+        }
+
+        if (left.second >= 0 && arr[left.first][left.second] == BLOCKED){
+            frontiers.push_back(left);
+        }
+
+        return frontiers;
+    } // implement finding of frontier cells. Cell inputted as pair and then return an array that contains frontier cells
+
+    void generateMaze() {
         /*
-    WHILE LIST OF FRONTIER IS NOT EMPTY{
-	-pick random value from list and set the value between frontier and random value from list to PASSAGE
-	-find frontier cells of random value from list and add them to list of frontier values
-	-remove frontier cell from frontier list.
-	}
-    */
+         * generate maze array
+         * pick random cell as starting point
+         * make starting point passage
+         * generate frontier cells for starting point and add to list of frontier cells
+         *
+         * WHILE LIST OF FRONTIER IS NOT EMPTY{
+         *  -pick random value from list and set the value between frontier and random value from list to PASSAGE
+         *  -find frontier cells of random value from list and add them to list of frontier values
+         *  -remove frontier cell from frontier list.
+         *  }
+         *
+         * optional: make last frontier cell as the end point
+        */
 
     }
 
